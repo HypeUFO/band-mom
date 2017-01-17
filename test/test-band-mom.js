a6ebeb21a2c36250e1edfc8bb62ae47cb838ae40
Michael Maio <michael.maio22@gmail.com>
2017-01-16 16:59:00 -0800

    add test and .travis.yml for deployment

---
+++ test/test-band-mom.js
@@ -0,0 +1,20 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const server = require('../server.js');
+
+const should = chai.should();
+
+const app = server.app;
+
+chai.use(chaiHttp);
+
+describe ('index page', function(){
+    it ('should load', function(){
+        return chai.request(app)
+        .get('/')
+        .then(function(res){
+            res.should.have.status(200);
+            res.should.be.html;
+        });
+    });
+});
