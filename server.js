b470a4d08a71a75eeada91b04809b549e6b68229
Michael Maio <michael.maio22@gmail.com>
2017-01-16 18:46:08 -0800

    add route for dashboard page

--- server.js
+++ server.js
@@ -4,6 +4,13 @@ var app = express();
 
 app.use(express.static('public'));
 
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+app.get('/dashboard', (req, res) => {
+  res.sendFile(__dirname + '/public/dashboard.html');
+});
 
 app.listen(process.env.PORT || 8080);
 
