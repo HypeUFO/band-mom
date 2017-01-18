911c7fbe5f738fde6617e23f0322357f6cb60908
Michael Maio <michael.maio22@gmail.com>
2017-01-18 12:37:50 -0800

    add 'create event guide with mock data'

--- public/app.js
+++ public/app.js
@@ -1,11 +1,8 @@
-
-
 // this is mock data, but when we create our API
 // we'll have it return data that looks like this
 
 const MOCK_EVENTS = {
-    "events": [
-        {
+    "events": [{
         "venueName": "The Rainbow Room",
         "venueAddress": "9015 W Sunset Blvd West Hollywood, CA 90069",
         "eventDate": "February 2, 2017",
@@ -84,6 +81,97 @@ const MOCK_USERS = {
     }]
 };
 
+// NEW EVENT SETUP GUIDE
+
+const QUESTIONS = {
+    currentQuestion: 0,
+    manifest: [],
+    questions: [{
+        question: "What is the date of the event?",
+    }, {
+        question: "What is the venue name?",
+    }, {
+        question: "What is the venue address?",
+    }, {
+        question: "What time does the event start?",
+    }, {
+        question: "What time is sound check?",
+    }, {
+        question: "Is there anything you need to purchase before the event?",
+        answer: [{
+            name: "quarter inch cables",
+            quantity: ""
+        }, {
+            name: "XLR cables",
+            quantity: ""
+        }, {
+            name: "guitar/bass strings",
+            quantity: ""
+        }, {
+            name: "misc",
+            description: "$('.description').val()",
+            quantity: ""
+        }]
+    }, {
+        question: "Are there any notes you want to add?",
+    }]
+};
+/*
+function doGuide() {
+    
+}*/
+
+function renderNextQuestion() {
+    $('.btn-guide-next').on('click', function (event) {
+        if (QUESTIONS.currentQuestion < QUESTIONS.questions.length && QUESTIONS.currentQuestion > 0) {
+            QUESTIONS.manifest.push($('.user-event-input').val());
+            var question = QUESTIONS.questions[QUESTIONS.currentQuestion];
+            var questionHtml = `<h3>${question.question}</h3><form class="form-field" name="answerList">`;
+            var prompt = `<input type="text" class="form-control user-event-input" placeholder="">`;
+            questionHtml += prompt;
+            questionHtml += `</form>`;
+            $('#event-guide-form').html(questionHtml);
+        } else if (QUESTIONS.currentQuestion === 0) {
+            var question = QUESTIONS.questions[QUESTIONS.currentQuestion];
+            var questionHtml = `<h3>${question.question}</h3><form class="form-field" name="answerList">`;
+            var prompt = `<input type="text" class="form-control user-event-input" placeholder="">`;
+            questionHtml += prompt;
+            questionHtml += `</form>`;
+            $('#event-guide-form').html(questionHtml);
+        }
+        QUESTIONS.currentQuestion++;
+
+        //evalProgress();
+        //createProgressTemplate();
+    });
+}
+
+function renderEvent() {
+    $('.btn-guide-next').on('click', function () {
+        if (QUESTIONS.currentQuestion > QUESTIONS.questions.length) {
+            $('table').append(
+                `<tr>
+                <td> ${QUESTIONS.manifest[0]} </td>
+                <td> ${QUESTIONS.manifest[1]} </td>
+                <td> ${QUESTIONS.manifest[2]} </td>
+                <td> ${QUESTIONS.manifest[3]} </td>
+                <td> ${QUESTIONS.manifest[4]} </td>
+            </tr>`);
+            console.log(QUESTIONS.manifest)
+            $('#eventGuideModal').modal('hide');
+            QUESTIONS.manifest = [];
+            QUESTIONS.currentQuestion = 0;
+        }
+    });
+}
+
+
+
+
+
+
+
+
 // GET USER EVENTS ON LOGIN
 
 // this function's name and argument can stay the
@@ -96,15 +184,17 @@ const MOCK_USERS = {
 function getEvents(callbackFn) {
     // we use a `setTimeout` to make this asynchronous
     // as it would be with a real AJAX call.
-	setTimeout(function(){ callbackFn(MOCK_EVENTS)}, 1);
+    setTimeout(function () {
+        callbackFn(MOCK_EVENTS)
+    }, 1);
 }
 
 // this function stays the same when we connect
 // to real API later
 function displayEvents(data) {
     for (index in data.events) {
-	   $('table').append(
-        `<tr>
+        $('table').append(
+            `<tr>
                 <td><a href="#"> ${data.events[index].eventDate} </a></td>
                 <td> ${data.events[index].venueName} </td>
                 <td> ${data.events[index].venueAddress} </td>
@@ -117,31 +207,31 @@ function displayEvents(data) {
 // this function can stay the same even when we
 // are connecting to real API
 function getAndDisplayEvents() {
-	getEvents(displayEvents);
+    getEvents(displayEvents);
 
 }
 
 // LOGIN BUTTON LINKS TO DASHBOARD
 
-const handleLogin = function() {
-$('.btn-login').click(function()
-{
-    window.location.href = "dashboard.html";
-})
+const handleLogin = function () {
+    $('.btn-login').click(function () {
+        window.location.href = "dashboard.html";
+    })
 };
 
-const handleSignUp = function() {
-$('.btn-signup').click(function()
-{
-    window.location.href = "dashboard.html";
-})
+const handleSignUp = function () {
+    $('.btn-signup').click(function () {
+        window.location.href = "dashboard.html";
+    })
 };
 
 
 
 //  on page load do this
-$(document).ready(function() {
+$(document).ready(function () {
     handleLogin();
     handleSignUp();
-	getAndDisplayEvents();
+    getAndDisplayEvents();
+    renderNextQuestion();
+    renderEvent();
 });
